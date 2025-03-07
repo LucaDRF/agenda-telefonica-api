e81b69193e65f39430ddfc5538556d2411b0b568
LucaDRF <lucadr.fiorenzano@gmail.com>
2025-03-07 02:13:37 -0300

    added repository methods

--- src/main/java/com/agenda_telefonica/repository/ContactRepository.java
+++ src/main/java/com/agenda_telefonica/repository/ContactRepository.java
@@ -3,7 +3,11 @@ package com.agenda_telefonica.repository;
 import com.agenda_telefonica.entity.ContactEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ContactRepository  extends JpaRepository<ContactEntity, UUID> {
+    List<ContactEntity> findAllByIsFavoriteTrue();
+
+    ContactEntity findByEmail(String email);
 }
