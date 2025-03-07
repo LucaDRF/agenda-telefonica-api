a08d77a380bbe01868022354b7bcb9650f3da453
LucaDRF <lucadr.fiorenzano@gmail.com>
2025-03-06 23:56:33 -0300

    added repository

--- src/main/java/com/agenda_telefonica/repository/ContactRepository.java
+++ src/main/java/com/agenda_telefonica/repository/ContactRepository.java
@@ -3,5 +3,7 @@ package com.agenda_telefonica.repository;
 import com.agenda_telefonica.entity.ContactEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContactRepository  extends JpaRepository<ContactEntity, Long> {
+import java.util.UUID;
+
+public interface ContactRepository  extends JpaRepository<ContactEntity, UUID> {
 }
