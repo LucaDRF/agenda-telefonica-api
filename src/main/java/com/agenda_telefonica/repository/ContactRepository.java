3b4fed1e349d3a28131d5774514453766cfe2b51
LucaDRF <lucadr.fiorenzano@gmail.com>
2025-03-11 23:58:27 -0300

    changed validation queries

--- src/main/java/com/agenda_telefonica/repository/ContactRepository.java
+++ src/main/java/com/agenda_telefonica/repository/ContactRepository.java
@@ -2,6 +2,7 @@ package com.agenda_telefonica.repository;
 
 import com.agenda_telefonica.entity.ContactEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
@@ -10,4 +11,10 @@ public interface ContactRepository  extends JpaRepository<ContactEntity, UUID> {
     List<ContactEntity> findAllByIsFavoriteTrue();
 
     ContactEntity findByEmail(String email);
+
+    @Query("SELECT contact FROM ContactEntity contact WHERE contact.cellPhone = :phone")
+    ContactEntity findExistingPhone(String phone);
+
+    @Query("SELECT contact FROM ContactEntity contact WHERE contact.id != :id AND contact.cellPhone = :phone")
+    ContactEntity findExistingPhoneById(UUID id, String phone);
 }
